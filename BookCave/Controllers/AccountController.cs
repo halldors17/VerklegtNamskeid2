adf290040af8149de98bb73c88c69b157465a3f4
root <root@LAPTOP-OL9PC8NL.localdomain>
2018-05-04 13:23:43 +0000

    log in view

--- BookCave/Controllers/AccountController.cs
+++ BookCave/Controllers/AccountController.cs
@@ -5,16 +5,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BookCave.Models;
+using BookCave.Models.ViewModels;
 
 namespace BookCave.Controllers
 {
     public class AccountController : Controller
     {
-       [HttpGet]
+        [HttpGet]
         public IActionResult Login()
         {
             ViewBag.Title = "Log In";
             return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel login)
+        {
+            if(ModelState.IsValid)
+            {
+                
+            }
+            return View(login);
         }
         public IActionResult Error()
         {
