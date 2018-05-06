3b5d2d55ed6d9ff87cda9d4a2174126b29118b70
Garðar Smári Vilhjálmsson <gassi-massi@hotmail.com>
2018-05-06 14:52:46 +0000

    .

--- BookCave/Models/ViewModels/RegisterViewModel.cs
+++ BookCave/Models/ViewModels/RegisterViewModel.cs
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-using System.ComponentModel.DataAnnotations;
-
-namespace BookCave.Models.ViewModels
-{
-    public class RegisterViewModel
-    {
-        [Required, Display(Name = "First Name")]
-        public string FirstName { get; set; }
-         [Required, Display(Name = "Last Name")]
-        public string LastName { get; set; }
-       [Required, EmailAddress, Display(Name = "Netfang")]
-        public string Email { get; set; }
-        [Required, MinLength(8), DataType(DataType.Password), Display(Name = "Lykilorð")]
-        public string Password { get; set; }
-        [Required, MinLength(8), DataType(DataType.Password), Display(Name = "Staðfesta Lykilorð")]
-        [Compare("Password", ErrorMessage = "Lykilorðið passar ekki")]
-        public string ConfirmPassword { get; set; }
-    }
-=======
 using System.ComponentModel.DataAnnotations;
 
 namespace BookCave.Models.ViewModels
@@ -40,5 +20,4 @@ namespace BookCave.Models.ViewModels
         [Compare("Password", ErrorMessage = "Lykilorðið passar ekki")]
         public string ConfirmPassword { get; set; }
     }
->>>>>>> ab827aff14233442d8edfb737c3ad2ee197fc1d2
 }
