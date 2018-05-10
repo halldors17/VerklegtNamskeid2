b6518a532ca1ba56f20dc188ee50d29750d94f4f
root <root@DESKTOP-BR1Q3HP.localdomain>
2018-05-10 23:05:44 +0000

    notandi getur sett inn mynd og uppáhalds bók

---
+++ BookCave/Migrations/AuthenticationDb/20180510222751_ExtendingAspNetUser2.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookCave.Migrations.AuthenticationDb
+{
+    public partial class ExtendingAspNetUser2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "age",
+                table: "AspNetUsers",
+                newName: "Age");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FavBook",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FavBook",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Age",
+                table: "AspNetUsers",
+                newName: "age");
+        }
+    }
+}
--- BookCave/Models/ApplicationUser.cs
+++ BookCave/Models/ApplicationUser.cs
@@ -6,6 +6,8 @@ namespace BookCave.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int age { get; set; }
+        public int Age { get; set; }
+        public string Image { get; set; }
+        public string FavBook { get; set; }
     }
 }
